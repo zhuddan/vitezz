abc3fddc89cea25c4870135152005e8be5efc6aa
zhudan <476182045@qq.com>
2022-06-29 10:16:23 +0800

    1st commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
+
+
+
+// test
+const names: Name[] = ['张三', '李四', '王二', '王二', '麻子'];
+console.log(names);
