99c48872f53d32bbe7d45e6dd0bc84b5028602f8
zhudan <476182045@qq.com>
2022-06-29 13:57:17 +0800

    fix: lint

--- src/utils/http/axiosTransform.ts
+++ src/utils/http/axiosTransform.ts
@@ -6,9 +6,9 @@ import type { AxiosRequestConfig, AxiosResponse } from 'axios';
 import type { RequestOptions, Result } from '~/axios';
 
 export interface CreateAxiosOptions extends AxiosRequestConfig {
-  authenticationScheme?: string
-  transform?: AxiosTransform
-  requestOptions?: RequestOptions
+  authenticationScheme?: string;
+  transform?: AxiosTransform;
+  requestOptions?: RequestOptions;
 }
 
 export abstract class AxiosTransform {
