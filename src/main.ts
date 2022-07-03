f521cd7f7b417c79c59f510775da55450f75bbc2
zhuddan <476182045@qq.com>
2022-07-03 14:36:29 +0800

    fix: cache

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,3 @@ import { store } from '@/store';
 import router from '@/router';
 
 createApp(App).use(store).use(router).mount('#app');
-
-import { useCache } from './hooks/web/useCache';
-
-const webCache = useCache();
-webCache.set('token', 'data');
--- src/utils/cache/index.ts
+++ src/utils/cache/index.ts
@@ -3,13 +3,13 @@ import { useCache } from '@/hooks/web/useCache';
 const webCache = useCache();
 
 export function setToken(value: string) {
-  webCache.set('token', value, { day: 7 });
+  webCache.set('TOKEN', value, { day: 7 });
 }
 
 export function getToken() {
-  return webCache.get('token');
+  return webCache.get('TOKEN');
 }
 
 export function removeToken() {
-  webCache.remove('token');
+  webCache.remove('TOKEN');
 }
