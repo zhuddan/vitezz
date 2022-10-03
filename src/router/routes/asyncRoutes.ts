9a18a727c846fdb7a8e50570cabdc16dae09532e
zhuddan <476182045@qq.com>
2022-10-04 01:52:41 +0800

    update

--- src/router/routes/asyncRoutes.ts
+++ src/router/routes/asyncRoutes.ts
@@ -1,20 +1,4 @@
 import type { RouteRecordRaw } from 'vue-router';
 
 // 动态路由
-export const asyncRoutes: RouteRecordRaw[] = [
-  {
-    path: '/list',
-    meta: {
-      title: '列表',
-      auth: false,
-    },
-    component: () => import('@/views/List.vue'),
-  },
-  {
-    path: '/detail/:id',
-    meta: {
-      title: '列表详情',
-    },
-    component: () => import('@/views/About.vue'),
-  },
-];
+export const asyncRoutes: RouteRecordRaw[] = [];
