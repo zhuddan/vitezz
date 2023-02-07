6891bd67a1e67d619392ba54d65cf78ae0f9a1f7
zhuddan <476182045@qq.com>
2023-02-08 00:25:44 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
 import Layout from '@/layout/index.vue';
+import { Button } from '@zhudddan/schema-form';
 </script>
 
 <template>
-  <Layout />
+  <Button type="default" size="large" />
 </template>
