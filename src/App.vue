3e473417bc873d846621478564e776603c24eb38
zhuddan <476182045@qq.com>
2023-02-16 20:08:58 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script lang="ts" setup>
 import Layout from '@/layout/index.vue';
-import { isArray } from '@vue/shared';
 </script>
 
 <template>
-  <Button type="default" size="large" />
+  <Layout />
 </template>
